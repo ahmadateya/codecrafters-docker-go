dfb4786d032e6634018810e504e0babba5236e7c
Ahmad Ateya <ahmad.m.ateya@gmail.com>
2023-03-25 00:22:05 +0200

    pass 3rd stage

--- app/main.go
+++ app/main.go
@@ -6,12 +6,17 @@ import (
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
-func main() {	
+func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
-	
 	cmd := exec.Command(command, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	_ = cmd.Run()
+	err := cmd.Run()
+	if err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			exitCode := exitError.ProcessState.ExitCode()
+			os.Exit(exitCode)
+		}
+	}
 }
